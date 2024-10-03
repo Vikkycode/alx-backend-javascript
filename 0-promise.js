03ca4209a24f9a00cfd085e7e05a04e6099e22ca
Victor <vicky.robilson5528@gmail.com>
2024-10-03 14:05:23 +0100

    0 promise

---
+++ 0-promise.js
@@ -0,0 +1,11 @@
+export default function getResponseAPI() {
+  return new Promise((resolve, reject) => {
+    const success = true;
+
+    if (success) {
+      resolve('success response from API');
+    } else {
+      reject(new Error('Error: failed to get response from API'));
+    }
+  });
+}
