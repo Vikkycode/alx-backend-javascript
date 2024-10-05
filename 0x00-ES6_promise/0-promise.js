e3d5a09978e929eaf8af773c7d681187082d2663
Victor Oricha <68545949+Vikkycode@users.noreply.github.com>
2024-10-05 06:10:14 +0100

    Create 0-promise.js

---
+++ 0x00-ES6_promise/0-promise.js
@@ -0,0 +1,10 @@
+export default function getResponseFromAPI(){
+    return new Promise((resolve,reject)=>{
+        const success = true
+        if (success){
+            resolve("Get response from API")
+        } else {
+            reject(new Error("Failed to get response from API"))
+        }
+    })
+}
