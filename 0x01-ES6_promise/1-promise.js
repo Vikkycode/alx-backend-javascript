e37e989147c69dbb9c1d9713b290d2fce2436864
Victor <vicky.robilson5528@gmail.com>
2024-10-05 11:47:52 +0100

    update 1-promise

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,12 +1,12 @@
-export default function getFullResponseFromAPI(isSuccess) {
-  return new Promise((resolve, reject) => {
-    if (isSuccess) {
+export default function getFullResponseFromAPI(success){
+  return new Promise((resolve, reject)=>{
+    if(success){
       resolve({
-        status: 200,
-        body: 'Success',
-      });
-    } else {
-      reject(new Error('The fake API is not working  currently'));
+        status:200,
+        body:'Success'
+      })
+    } else{
+      reject(new Error('The fake API is not working currently'))
     }
-  });
+  })
 }
