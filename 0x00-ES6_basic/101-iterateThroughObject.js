85a54500ace3e35d4b4932e5e891208507d79fe5
Victor <vicky.robilson5528@gmail.com>
2024-10-05 08:26:54 +0100

    remove 0x01 es6 promise

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,9 +1,13 @@
+// export default function iterateThroughObject(reportWithIterator) {
+//   const employeeNames = [];
+
+//   for (const employee of reportWithIterator) {
+//     employeeNames.push(employee);
+//   }
+
+//   return employeeNames.join(' | ');
+// }
 export default function iterateThroughObject(reportWithIterator) {
-  const employeeNames = [];
-
-  for (const employee of reportWithIterator) {
-    employeeNames.push(employee);
-  }
-
-  return employeeNames.join(' | ');
+  // If `reportWithIterator` is indeed an iterator, just collect the names and join
+  return [...reportWithIterator].join(' | ');
 }
