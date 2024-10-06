1cc484c7ab218ed055aa22f4bd98d3035a86f9c7
Victor <vicky.robilson5528@gmail.com>
2024-10-06 06:02:25 +0100

    9 try

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -2,12 +2,12 @@ export default function guardrail(mathFunction) {
   const queue = [];
 
   try {
-    const results = mathFunction();
-    queue.push(results);
+    const result = mathFunction();  // Execute the math function
+    queue.push(result);             // Push the result of the function to the queue
   } catch (error) {
-    queue.push(error.message);
+    queue.push(error.message);      // Push the error message in case of an exception
   } finally {
-    queue.push('guard was processed');
+    queue.push('Guardrail was processed');  // Always push this message to the queue
   }
 
   return queue;
