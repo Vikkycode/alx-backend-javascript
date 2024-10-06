308de7f5741952cf61f0e4dcdb32732bf8e79455
Victor <vicky.robilson5528@gmail.com>
2024-10-06 05:59:33 +0100

    6 final user

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,15 +2,18 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
+  // Create an array of promises
   const promises = [
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
+    signUpUser(firstName, lastName), // Resolves to user data
+    uploadPhoto(fileName), // Rejects due to photo upload failure
   ];
 
+  // Wait for all promises to settle
   const results = await Promise.allSettled(promises);
 
+  // Map the results and return the required output format
   return results.map((result) => ({
     status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason,
+    value: result.status === 'fulfilled' ? result.value : result.reason, // If fulfilled, return value; else return reason (error)
   }));
 }
