b79fcf11d60be1855da191cb000c45faa6268cfc
Victor <vicky.robilson5528@gmail.com>
2024-10-09 01:01:09 +0100

    typed arrays

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,10 +1,11 @@
 export default function createInt8TypedArray(length, position, value) {
-  if (position < 0 || position >= length) {
-    throw Error('Position outside range');
+    if (position < 0 || position >= length) {
+      throw Error('Position outside range');
+    }
+  
+    const buffer = new ArrayBuffer(length);
+    const int8View = new Int8Array(buffer);
+    int8View[position] = value;
+    return buffer;
   }
-
-  const buffer = new ArrayBuffer(length);
-  const int8View = new Int8Array(buffer);
-  int8View[position] = value;
-  return buffer;
-}
+  
