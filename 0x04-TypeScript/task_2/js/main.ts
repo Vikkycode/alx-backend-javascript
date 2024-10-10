f406a48cbacc095e09d0b0ae9af1233ba122af94
Victor <vicky.robilson5528@gmail.com>
2024-10-10 03:27:53 +0100

    task 2

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,91 @@
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+// Classes
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return "Working from home";
+  }
+
+  getCoffeeBreak(): string {
+    return "Getting a coffee break";
+  }
+
+  workDirectorTasks(): string {
+    return "Getting to director tasks";
+  }
+}
+
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return "Cannot work from home";
+  }
+
+  getCoffeeBreak(): string {
+    return "Cannot have a break";
+  }
+
+  workTeacherTasks(): string {
+    return "Getting to work";
+  }
+}
+
+// Function to create employee based on salary
+function createEmployee(salary: number | string): Director | Teacher {
+  if (typeof salary === 'number' && salary < 500) {
+    return new Teacher();
+  } else {
+    return new Director();
+  }
+}
+
+
+console.log(createEmployee(200)); 
+console.log(createEmployee(1000)); 
+console.log(createEmployee('$500'));
+
+
+// Type predicate function
+function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+// Function to execute work based on employee type
+function executeWork(employee: Director | Teacher): void {
+  if (isDirector(employee)) {
+    console.log(employee.workDirectorTasks());
+  } else {
+    console.log(employee.workTeacherTasks());
+  }
+}
+
+executeWork(createEmployee(200)); 
+executeWork(createEmployee(1000)); 
+
+
+// String literal type for subjects
+type Subjects = 'Math' | 'History';
+
+// Function to determine the class being taught
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  } else if (todayClass === 'History') {
+    return 'Teaching History';
+  } else {
+    // a default case or throw an error here
+    return 'Teaching a different subject';
+  }
+}
+
+console.log(teachClass('Math')); 
+console.log(teachClass('History')); 
